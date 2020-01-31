685cb0f49433badfd3a00781b3f27368ab299d61
RayhanHamada <hamadadesk1999@gmail.com>
2020-01-31 19:42:01 +0700

    add MyDispatch to types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,5 @@
 import { StateType, ActionType } from 'typesafe-actions';
+import { Dispatch } from 'redux';
 
 declare namespace MyTypes {
 	export type RootAction = ActionType<
@@ -8,6 +9,8 @@ declare namespace MyTypes {
 		ReturnType<typeof import('./store/rootReducer').default>
 	>;
 	export type Store = StateType<ReturnType<typeof import('./store').default>>;
+
+	export type MyDispatch = Dispatch<RootAction>;
 }
 
 declare module 'typesafe-actions' {
